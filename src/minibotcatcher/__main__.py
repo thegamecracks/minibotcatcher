5ff0f4d003db262c8803bbb82d024d546c66a5e1
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-06-30 19:16:01 -0400

    feat: add bot instance and startup

--- src/minibotcatcher/__main__.py
+++ src/minibotcatcher/__main__.py
@@ -1,5 +1,24 @@
+import os
+import re
+import sys
+
+
 def main() -> None:
-    print("Hello world!")
+    token = read_token()
+
+    from minibotcatcher.bot import Bot  # defer discord.py import
+
+    bot = Bot()
+    bot.run(token)
+
+
+def read_token() -> str:
+    token = os.environ.pop("BOT_TOKEN", "").strip()
+    if not token:
+        sys.exit("BOT_TOKEN environment variable must be set")
+    elif not re.fullmatch(r"\w+\.\w+\.\S+", token):
+        sys.exit("BOT_TOKEN appears to be invalid, double check bot token")
+    return token
 
 
 if __name__ == "__main__":
---
+++ src/minibotcatcher/bot.py
@@ -0,0 +1,68 @@
+import importlib.metadata
+import logging
+import sys
+from contextlib import suppress
+
+import discord
+from discord.ext import commands
+
+log = logging.getLogger(__name__)
+
+
+class Bot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned,
+            help_command=None,
+            intents=discord.Intents(
+                guilds=True,
+                messages=True,
+            ),
+            max_messages=None,
+            strip_after_prefix=True,
+        )
+
+    async def login(self, token: str) -> None:
+        try:
+            return await super().login(token)
+        except discord.LoginFailure:
+            log.critical("Discord refused our login! Please check BOT_TOKEN.")
+            sys.exit(1)
+        except discord.PrivilegedIntentsRequired:
+            log.critical(
+                "Missing privileged intents! "
+                "You must enable the Message Content intent."
+            )
+            sys.exit(1)
+
+    async def setup_hook(self) -> None:
+        await self._maybe_load_jishaku()
+
+        invite_link = self.get_standard_invite()
+        log.info("Invite link:\n    %s", invite_link)
+
+    async def _maybe_load_jishaku(self) -> None:
+        with suppress(importlib.metadata.PackageNotFoundError):
+            version = importlib.metadata.version("jishaku")
+            await self.load_extension("jishaku")
+            log.info("Loaded jishaku extension (v%s)", version)
+
+    def get_standard_invite(self) -> str:
+        assert self.application is not None
+        return discord.utils.oauth_url(
+            self.application.id,
+            scopes=("bot",),
+            permissions=discord.Permissions(
+                read_messages=True,
+                send_messages=True,
+                send_messages_in_threads=True,
+                embed_links=True,
+                attach_files=True,
+                # Dangerous permissions
+                manage_messages=True,
+                moderate_members=True,
+            ),
+        )
+
+
+class Context(commands.Context[Bot]): ...
