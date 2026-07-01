f7739f971fd7ec568bc9a2cbd1fa94d55ae92aa8
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-07-01 18:44:08 -0400

    style: reformat filters.py

--- src/minibotcatcher/antispam/filters.py
+++ src/minibotcatcher/antispam/filters.py
@@ -215,7 +215,9 @@ def check_mention_spam(
 
 def _message_contains_any_mention(message: discord.Message) -> int:
     return (
-        any(user for user in message.mentions if not user.bot and user != message.author)
+        any(
+            user for user in message.mentions if not user.bot and user != message.author
+        )
         or any(role for role in message.role_mentions if role.mentionable)
         or "@everyone" in message.content
         or "@here" in message.content
