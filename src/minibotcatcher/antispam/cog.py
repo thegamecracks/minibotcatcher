79c47addcca618765c3dda80fe23fb6345c8932d
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-07-01 18:15:51 -0400

    feat: initial implementation of spam filters

--- src/minibotcatcher/antispam/cog.py
+++ src/minibotcatcher/antispam/cog.py
@@ -1,9 +1,174 @@
+import datetime
+import logging
+from typing import Callable
+
 import discord
 from discord.ext import commands
 
 from minibotcatcher.bot import Bot
 
+from .filters import (
+    SpamContext,
+    SpamContextCache,
+    SpamDetection,
+    check_burst_spam,
+    check_mention_spam,
+)
+
+ADMIN_PERMISSIONS = discord.Permissions(
+    kick_members=True,
+    ban_members=True,
+    administrator=True,
+    manage_channels=True,
+    manage_guild=True,
+    manage_messages=True,
+    mute_members=True,
+    deafen_members=True,
+    move_members=True,
+    manage_nicknames=True,
+    manage_roles=True,
+    manage_webhooks=True,
+    manage_expressions=True,
+    manage_events=True,
+    manage_threads=True,
+    moderate_members=True,
+    bypass_slowmode=True,
+)
+
+log = logging.getLogger(__name__)
+
+
+def _is_mod_role(role: discord.Role) -> bool:
+    return (
+        not role.is_default()
+        and not role.is_integration()
+        and not role.is_bot_managed()
+        and bool(
+            role.permissions
+            & discord.Permissions(
+                kick_members=True,
+                ban_members=True,
+            )
+        )
+        and (role.mentionable or role.guild.me.guild_permissions.mention_everyone)
+    )
+
 
 class AntiSpam(commands.Cog):
+    filters: list[Callable[[SpamContext], SpamDetection | None]]
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.context_cache = SpamContextCache(
+            message_period=datetime.timedelta(minutes=10),
+        )
+        self.filters = [
+            check_burst_spam,
+            check_mention_spam,
+        ]
+
+    @commands.Cog.listener("on_message")
+    async def apply_spam_filters(self, message: discord.Message) -> None:
+        if message.guild is None:
+            return
+
+        assert isinstance(message.author, discord.Member)
+        if self.is_admin(message.author):
+            return
+
+        context = self.context_cache.get(message)
+        if context is None:
+            return
+
+        for func in self.filters:
+            detection = func(context)
+            if detection is not None:
+                await self.take_action_on_detection(detection)
+                self.context_cache.pop(message)
+                return
+
+    def is_admin(self, author: discord.Member) -> bool:
+        if author.top_role >= author.guild.me.top_role:
+            return True
+        elif author.guild_permissions & ADMIN_PERMISSIONS:
+            return True
+        return False
+
+    async def take_action_on_detection(self, detection: SpamDetection) -> None:
+        timed_out_until = await self.timeout_offender(detection)
+        await self.send_audit_message(detection, timed_out_until=timed_out_until)
+        await self.delete_offending_messages(detection)
+
+    async def timeout_offender(
+        self, detection: SpamDetection
+    ) -> datetime.datetime | None:
+        if not detection.guild.me.guild_permissions.moderate_members:
+            return
+
+        duration = datetime.timedelta(minutes=10)
+        timed_out_until = discord.utils.utcnow() + duration
+        log.info(
+            "Timing out %s-%s for %s",
+            detection.guild.id,
+            detection.author.id,
+            duration,
+        )
+        await detection.author.timeout(timed_out_until, reason=detection.reason)
+        return timed_out_until
+
+    async def send_audit_message(
+        self,
+        detection: SpamDetection,
+        *,
+        timed_out_until: datetime.datetime | None,
+    ) -> None:
+        channel = detection.audit_channel
+        if channel is None:
+            return
+
+        content = []
+
+        mention = detection.author.mention
+        if timed_out_until is not None:
+            timestamp_f = discord.utils.format_dt(timed_out_until, style="f")
+            content.append(
+                f"⚠️ {mention} has been timed out until {timestamp_f} "
+                f"(reason: {detection.reason})."
+            )
+        else:
+            content.append(
+                f"⚠️ {mention} triggered a spam filter (reason: {detection.reason})."
+            )
+
+        mod_role = self.get_mod_role(detection.guild)
+        if mod_role is not None:
+            has = "has" if not mod_role.name.endswith("s") else "have"
+            content.append("")
+            content.append(f"The {mod_role.mention} {has} been alerted for review.")
+
+        log.info("Sending audit message to %s-%s", detection.guild.id, channel.id)
+        await channel.send(
+            "\n".join(content),
+            allowed_mentions=discord.AllowedMentions(everyone=False, users=False),
+        )
+
+    async def delete_offending_messages(self, detection: SpamDetection) -> None:
+        deleteable = [
+            message
+            for message in detection.messages
+            if message.channel.permissions_for(detection.guild.me).manage_messages
+        ]
+        if not deleteable:
+            return
+
+        log.info(
+            "Deleting %d messages from %s-%s",
+            len(deleteable),
+            detection.guild.id,
+            detection.author.id,
+        )
+        for message in deleteable:
+            await message.delete(delay=0)
+
+    def get_mod_role(self, guild: discord.Guild) -> discord.Role | None:
+        return discord.utils.find(_is_mod_role, guild.roles)
---
+++ src/minibotcatcher/antispam/filters.py
@@ -0,0 +1,224 @@
+from __future__ import annotations
+
+import bisect
+import datetime
+import logging
+import re
+from collections import deque
+from dataclasses import dataclass, field
+from typing import Sequence
+
+import discord
+
+URL_PATTERN = re.compile(r"https?://\w+\.\w+\S*")  # close enough :)
+
+log = logging.getLogger(__name__)
+
+
+class SpamContextCache:
+    """Create and manage SpamContext instances from messages.
+
+    :param message_period: The period for which messages are stored.
+
+    """
+
+    type KEY = tuple[int, int]
+    contexts: dict[KEY, SpamContext]
+
+    def __init__(self, *, message_period: datetime.timedelta) -> None:
+        self.contexts = {}
+        self.message_period = message_period
+
+    def get(self, message: discord.Message) -> SpamContext | None:
+        """Get the current SpamContext for a given message.
+
+        If there is a cached context, any expired messages
+        are removed. The passed message is then added to the context.
+
+        If the message is not associated with a guild,
+        this will always return None.
+
+        """
+        if message.guild is None:
+            return
+
+        self._prune_contexts()  # is this expensive?
+
+        key = (message.guild.id, message.author.id)
+        context = self.contexts.get(key)
+        if context is None:
+            assert isinstance(message.author, discord.Member)
+            context = SpamContext(author=message.author)
+            self.contexts[key] = context
+
+        context.add_message(message)
+        return context
+
+    def pop(self, message: discord.Message) -> SpamContext | None:
+        """Remove and return any SpamContext corresponding to the given message."""
+        if message.guild is None:
+            return
+
+        key = (message.guild.id, message.author.id)
+        return self.contexts.pop(key, None)
+
+    def _prune_contexts(self) -> None:
+        to_remove: list[SpamContextCache.KEY] = []
+        expires_at = discord.utils.utcnow() - self.message_period
+        for key, context in self.contexts.items():
+            context.remove_messages_before(expires_at)
+            if not context.messages:
+                to_remove.append(key)
+
+
+@dataclass(kw_only=True)
+class SpamContext:
+    """The context related to a member for purposes of spam detection."""
+
+    author: discord.Member
+    """The member to make a ruling on."""
+    _messages: deque[discord.Message] = field(default_factory=deque, init=False)
+
+    @property
+    def messages(self) -> Sequence[discord.Message]:
+        """All recent messages sorted from oldest to newest."""
+        return self._messages
+
+    @property
+    def guild(self) -> discord.Guild:
+        """The guild that the author is in."""
+        return self.author.guild
+
+    def add_message(self, message: discord.Message) -> None:
+        """Add a message to this context."""
+        if message.author != self.author:
+            raise ValueError("Cannot add message from a different author")
+
+        bisect.insort_right(
+            self._messages,
+            message,
+            key=lambda m: m.created_at,
+        )
+
+    def query_messages(self, *, after: datetime.datetime) -> list[discord.Message]:
+        """Filter messages based on the given parameters."""
+        return [m for m in self.messages if m.created_at >= after]
+
+    def remove_messages_before(self, expires_at: datetime.datetime) -> None:
+        """Remove messages older than the given datetime."""
+        messages = self._messages
+        while messages and messages[0].created_at < expires_at:
+            messages.popleft()
+
+
+@dataclass(kw_only=True)
+class SpamDetection:
+    """The result of a spam filter determining an author to have one or more
+    offending messages.
+    """
+
+    author: discord.Member
+    """The member to take action on."""
+    messages: list[discord.Message]
+    """A list of messages from the author."""
+    reason: str
+    """The reason why the messages were determined as spam."""
+
+    def __post_init__(self) -> None:
+        if not self.messages:
+            raise ValueError("Must have at least one message")
+        elif any(m.author != self.author for m in self.messages):
+            raise ValueError("Cannot include messages from another author")
+        self.messages.sort(key=lambda m: m.created_at)
+
+    @property
+    def guild(self) -> discord.Guild:
+        """The guild that the author is in."""
+        return self.author.guild
+
+    @property
+    def audit_channel(self) -> discord.abc.MessageableChannel | None:
+        """The channel of the most recent offending message that the bot can send to."""
+        return discord.utils.find(
+            _can_send_in_channel,
+            reversed([m.channel for m in self.messages]),
+        )
+
+
+def _can_send_in_channel(channel: discord.abc.MessageableChannel) -> bool:
+    assert channel.guild is not None
+    return channel.permissions_for(channel.guild.me).send_messages
+
+
+def check_burst_spam(
+    context: SpamContext,
+    *,
+    message_threshold: int = 4,
+    period: datetime.timedelta = datetime.timedelta(seconds=10),
+) -> SpamDetection | None:
+    """Check if an author has sent too many messages across different channels
+    in the given period.
+    """
+    after = discord.utils.utcnow() - period
+    messages = context.query_messages(after=after)
+    unique_channels = set(m.channel.id for m in messages)
+    if len(unique_channels) < message_threshold:
+        return
+
+    log.info(
+        "Detected burst message spam: %s-%s sent %d messages in %d channels "
+        "within a period of %s",
+        context.guild.id,
+        context.author.id,
+        len(messages),
+        len(unique_channels),
+        period,
+    )
+    return SpamDetection(
+        author=context.author,
+        messages=messages,
+        reason="burst spam - sent {len(messages)} messages",
+    )
+
+
+def check_mention_spam(
+    context: SpamContext,
+    *,
+    message_threshold: int = 3,
+    period: datetime.timedelta = datetime.timedelta(seconds=30),
+) -> SpamDetection | None:
+    """Check if an author has used a mention across too many messages
+    in the given period.
+
+    Multiple mentions in a single message only counts as one against the threshold.
+    Discord AutoMod can be used to limit the total number of mentions in a single message.
+
+    """
+    after = discord.utils.utcnow() - period
+    messages = context.query_messages(after=after)
+    messages = [m for m in messages if _message_contains_any_mention(m)]
+    if len(messages) < message_threshold:
+        return
+
+    log.info(
+        "Detected mention spam: %s-%s used mentions across %d messages "
+        "within a period of %s",
+        context.guild.id,
+        context.author.id,
+        len(messages),
+        period,
+    )
+    return SpamDetection(
+        author=context.author,
+        messages=messages,
+        reason=f"mention spam - sent {len(messages)} messages",
+    )
+
+
+def _message_contains_any_mention(message: discord.Message) -> int:
+    return (
+        len(message.raw_mentions) > 0
+        or len(message.raw_role_mentions) > 0
+        or "@everyone" in message.content
+        or "@here" in message.content
+    )
